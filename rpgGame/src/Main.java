aee11dae86092b1ddcd183e9eec269ac1a003871
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-05-23 19:51:48 -0400

    Update Main.java

--- rpgGame/src/Main.java
+++ rpgGame/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
     private JComboBox cboWeapons;
     private JComboBox cboPotions;
 
-    private Player player;
+    private final Player player;
 
     public static void main(String[] args) {
         // Creating Player
@@ -59,6 +59,10 @@ public class Main {
 
     public Main(Player player) {
         this.player = player;
+
+
+//        rtbMessages.setText("");
+//        rtbLocation.setText("");
 
         lblLevel.setText(String.valueOf(player.getLevel()));
         lblGold.setText(String.valueOf(player.getGold()));
@@ -166,13 +170,138 @@ public class Main {
         rtbLocation.setText(newlocation.Name + "\n" + newlocation.Description);
 
         // Completely heal player
-        player.CurerntHitPoints = player.MaxHitPoints;
+        player.CurrentHitPoints = player.MaximumHitPoints;
 
         // Update Hit points on UI
-        lblHitPoints.setText(CurrentHitPoints.toString());
+        lblHitPoints.setText(String.valueOf(player.CurrentHitPoints));
 
         // Check if the location has a quest
+        if(newlocation.QuestAvailableHere != null) {
+            // See if the player has a quest and if is it completed
+            boolean playerAlreadyHasQuest = false;
+            boolean playerAlreadyCompletedQuest = false;
 
+            for(PlayerQuest playerQuest: player.Quests) {
+                if(playerQuest.Details.ID == newlocation.QuestAvailableHere.ID) {
+                    playerAlreadyHasQuest = true;
+
+                    if(playerQuest.IsCompleted){
+                        playerAlreadyCompletedQuest = true;
+                    }
+                }
+            }
+
+            // See if the player already has a quest
+            if(playerAlreadyHasQuest) {
+                // If player has not already completed the quest
+                if(!playerAlreadyCompletedQuest) {
+                    // See if the player has all items needed to complete quest
+                    boolean playerHasAllItemsToCompleteQuest = true;
+
+                    for(QuestCompletionItem qci : newlocation.QuestAvailableHere.QuestCompletionItems) {
+                        boolean foundItemInPlayersInventory = false;
+
+                        // Check each item in the player's inventory to see if they have it and enough of it
+                        for (InventoryItem ii: player.Inventory) {
+                            // The player has this item in their inventory
+                            if(ii.Details == qci.Details) {
+                                foundItemInPlayersInventory = true;
+
+                                if(ii.Quantity == qci.Quantity) {
+                                    playerHasAllItemsToCompleteQuest = false;
+                                    break;
+                                }
+                                break;
+                            }
+                        }
+
+
+                        // If the required item wa not found set variable and stop looking
+                        if(!foundItemInPlayersInventory) {
+                            playerHasAllItemsToCompleteQuest = false;
+                            break;
+                        }
+                    }
+
+                    // Player has all items required to complete quest
+                    if(playerHasAllItemsToCompleteQuest) {
+                        // Display message
+                        rtbMessages.setText("You complete the '" + newlocation.QuestAvailableHere.Name + "' quest.");
+
+                        // Remove quests items from inventory
+                        for(QuestCompletionItem qci : newlocation.QuestAvailableHere.QuestCompletionItems) {
+                            for(InventoryItem ii: player.Inventory) {
+                                if(ii.Details == qci.Details) {
+                                    // Subtract quantity from players inventory that was needed to complete the quest
+                                    ii.Quantity -= qci.Quantity;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Give Quest Rewards
+                        rtbMessages.setText("You receive: \n" +
+                                newlocation.QuestAvailableHere.RewardExperiencePoints + " experience points\n"
+                                + newlocation.QuestAvailableHere.getRewardGold() + " gold\n"
+                                + newlocation.QuestAvailableHere.RewardItem.Name
+                        );
+
+                        player.ExperiencePoints += newlocation.QuestAvailableHere.RewardExperiencePoints;
+                        player.Gold += newlocation.QuestAvailableHere.RewardGold;
+
+                        // Add reward item to player inventory
+                        boolean addedItemToPlayerInventory = false;
+
+                        for(InventoryItem ii: player.Inventory) {
+                            if(ii.Details.ID == newlocation.QuestAvailableHere.RewardItem.ID){
+                                // They have item in inventory so increase quantity by 1
+                                ii.Quantity++;
+                                addedItemToPlayerInventory = true;
+                                break;
+                            }
+                        }
+
+                        // They didnt have the item, add it to their inventory with quality on 1
+                        if(!addedItemToPlayerInventory) {
+                            player.Inventory.add(new InventoryItem(newlocation.QuestAvailableHere.RewardItem, 1));
+                        }
+
+                        // Mark Quests as completed
+                        // Find quests in players quest list
+                        for(PlayerQuest pq: player.Quests) {
+                            if(pq.Details.ID == newlocation.QuestAvailableHere.ID) {
+                                // Mark as completed
+                                pq.IsCompleted = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                // The player does not already have the quest
+                // Display Message
+                rtbMessages.setText(
+                        "You receive the " + newlocation.QuestAvailableHere.Name + " quest.\n"
+                                + newlocation.QuestAvailableHere.Description + "\n"
+                                + "To complete it, return with:"
+                );
+
+                for(QuestCompletionItem qci : newlocation.QuestAvailableHere.QuestCompletionItems) {
+                    if(qci.Quantity == 1){
+                        rtbMessages.setText(qci.Quantity + " " + qci.Details.Name);
+                    }
+                    else {
+                        rtbMessages.setText(qci.Quantity + " " + qci.Details.NamePlural);
+                    }
+                }
+                // Add quest to the players quest list
+                player.Quests.add(new PlayerQuest(newlocation.QuestAvailableHere));
+            }
+
+
+
+        }
 
 
     }
