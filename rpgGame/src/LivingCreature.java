a6b390e306d97890244ad46324ed0e03fb158d2d
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-06-04 14:42:01 -0400

    Update LivingCreature.java

--- rpgGame/src/LivingCreature.java
+++ rpgGame/src/LivingCreature.java
@@ -3,12 +3,22 @@ public class LivingCreature {
     public int MaximumHitPoints;
 
     // Getters
-    public int getCurrentHitPoints() {return CurrentHitPoints;}
-    public int getMaximumHitPoints() {return MaximumHitPoints;}
+    public int getCurrentHitPoints() {
+        return CurrentHitPoints;
+    }
+
+    public int getMaximumHitPoints() {
+        return MaximumHitPoints;
+    }
 
     // Setters
-    public void setCurrentHitPoints(int CurrentHitPoints) {this.CurrentHitPoints = CurrentHitPoints;}
-    public void setMaximumHitPoints(int MaximumHitPoints) {this.MaximumHitPoints = MaximumHitPoints;}
+    public void setCurrentHitPoints(int CurrentHitPoints) {
+        this.CurrentHitPoints = CurrentHitPoints;
+    }
+
+    public void setMaximumHitPoints(int MaximumHitPoints) {
+        this.MaximumHitPoints = MaximumHitPoints;
+    }
 
     // Constructor
     public LivingCreature(int CurrentHitPoints, int MaximumHitPoints) {
