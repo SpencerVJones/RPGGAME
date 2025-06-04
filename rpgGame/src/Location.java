1c13b5b094b2b39f2aaf8eeae70d391d969fa85a
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-06-04 14:42:29 -0400

    Update Location.java

--- rpgGame/src/Location.java
+++ rpgGame/src/Location.java
@@ -12,30 +12,86 @@ public class Location {
     public Location LocationToWest;
 
     // Getters
-    public int getID() {return ID;}
-    public String getName() {return Name;}
-    public String getDescription() {return Description;}
+    public int getID() {
+        return ID;
+    }
 
-    public Item getItemRequiredToEnter() { return ItemRequiredToEnter; }
-    public Quest getQuestAvailableHere() { return QuestAvailableHere; }
-    public Monster getMonsterLivingHere() { return MonsterLivingHere; }
-    public Location getLocationToNorth() { return LocationToNorth; }
-    public Location getLocationToSouth() { return LocationToSouth; }
-    public Location getLocationToEast() { return LocationToEast; }
-    public Location getLocationToWest() { return LocationToWest; }
+    public String getName() {
+        return Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public Item getItemRequiredToEnter() {
+        return ItemRequiredToEnter;
+    }
+
+    public Quest getQuestAvailableHere() {
+        return QuestAvailableHere;
+    }
+
+    public Monster getMonsterLivingHere() {
+        return MonsterLivingHere;
+    }
+
+    public Location getLocationToNorth() {
+        return LocationToNorth;
+    }
+
+    public Location getLocationToSouth() {
+        return LocationToSouth;
+    }
+
+    public Location getLocationToEast() {
+        return LocationToEast;
+    }
+
+    public Location getLocationToWest() {
+        return LocationToWest;
+    }
 
     // Setters
-    public void setID(int ID) {this.ID = ID;}
-    public void setName(String name) {this.Name = name;}
-    public void setDescription(String description) {this.Description = description;}
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 
-    public void setItemRequiredToEnter(Item itemRequiredToEnter) {this.ItemRequiredToEnter = itemRequiredToEnter;}
-    public void setQuestAvailableHere(Quest questAvailableHere) {this.QuestAvailableHere = questAvailableHere;}
-    public void setMonsterLivingHere(Monster monsterLivingHere) {this.MonsterLivingHere = monsterLivingHere;}
-    public void setLocationToNorth(Location locationToNorth) {this.LocationToNorth = locationToNorth;}
-    public void setLocationToSouth(Location locationToSouth) {this.LocationToSouth = locationToSouth;}
-    public void setLocationToEast(Location locationToEast) {this.LocationToEast = locationToEast; }
-    public void setLocationToWest(Location locationToWest) {this.LocationToWest = locationToWest; }
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public void setDescription(String description) {
+        this.Description = description;
+    }
+
+    public void setItemRequiredToEnter(Item itemRequiredToEnter) {
+        this.ItemRequiredToEnter = itemRequiredToEnter;
+    }
+
+    public void setQuestAvailableHere(Quest questAvailableHere) {
+        this.QuestAvailableHere = questAvailableHere;
+    }
+
+    public void setMonsterLivingHere(Monster monsterLivingHere) {
+        this.MonsterLivingHere = monsterLivingHere;
+    }
+
+    public void setLocationToNorth(Location locationToNorth) {
+        this.LocationToNorth = locationToNorth;
+    }
+
+    public void setLocationToSouth(Location locationToSouth) {
+        this.LocationToSouth = locationToSouth;
+    }
+
+    public void setLocationToEast(Location locationToEast) {
+        this.LocationToEast = locationToEast;
+    }
+
+    public void setLocationToWest(Location locationToWest) {
+        this.LocationToWest = locationToWest;
+    }
 
 
     // Constructor with 3 parameters
@@ -45,7 +101,7 @@ public class Location {
         this.Description = description;
     }
 
-    // Constructor with 4 parameters (fixes your error)
+    // Constructor with 4 parameters
     public Location(int id, String name, String description, Item itemRequiredToEnter) {
         this.ID = id;
         this.Name = name;
