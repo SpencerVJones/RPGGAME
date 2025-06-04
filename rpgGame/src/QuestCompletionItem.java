eee66b5265d9c9551be669b57239b4ec362b66ce
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-06-04 14:49:41 -0400

    Update QuestCompletionItem.java

--- rpgGame/src/QuestCompletionItem.java
+++ rpgGame/src/QuestCompletionItem.java
@@ -3,15 +3,25 @@ public class QuestCompletionItem {
     public int Quantity;
 
     // Getters
-    public Item getDetails() {return Details;}
-    public int getQuantity() {return Quantity;}
+    public Item getDetails() {
+        return Details;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
 
     // Setters
-    public void setDetails(Item details) {this.Details = details;}
-    public void setQuantity(int quantity) {this.Quantity = quantity;}
+    public void setDetails(Item details) {
+        this.Details = details;
+    }
+
+    public void setQuantity(int quantity) {
+        this.Quantity = quantity;
+    }
 
     // Constructor
-    public QuestCompletionItem(Item details, int quantity){
+    public QuestCompletionItem(Item details, int quantity) {
         this.Details = details;
         this.Quantity = quantity;
     }
