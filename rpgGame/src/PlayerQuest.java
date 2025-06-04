9087766b7c4785076e14eeb0d7ac7bafd9cda429
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-06-04 14:48:40 -0400

    Update PlayerQuest.java

--- rpgGame/src/PlayerQuest.java
+++ rpgGame/src/PlayerQuest.java
@@ -3,15 +3,25 @@ public class PlayerQuest {
     public boolean IsCompleted;
 
     // Getters
-    public Quest getDetails(){return Details;}
-    public boolean IsCompleted(){return IsCompleted;}
+    public Quest getDetails() {
+        return Details;
+    }
+
+    public boolean IsCompleted() {
+        return IsCompleted;
+    }
 
     // Setters
-    public void setDetails(Quest details){this.Details = details;}
-    public void setCompleted(){this.IsCompleted = false;}
+    public void setDetails(Quest details) {
+        this.Details = details;
+    }
+
+    public void setCompleted() {
+        this.IsCompleted = false;
+    }
 
     // Constructor
-    public PlayerQuest(Quest details, boolean IsCompleted){
+    public PlayerQuest(Quest details, boolean IsCompleted) {
         this.Details = details;
         this.IsCompleted = IsCompleted;
     }
