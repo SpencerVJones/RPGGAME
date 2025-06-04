d2e6e5e03e12e7c6267efd7e4232ea94cf5e6b3a
Spencer Jones <154095060+SpencerVJones@users.noreply.github.com>
2025-06-04 14:41:07 -0400

    Update InventoryItem.java

--- rpgGame/src/InventoryItem.java
+++ rpgGame/src/InventoryItem.java
@@ -1,14 +1,24 @@
-public class InventoryItem {
+ppublic class InventoryItem {
     public Item Details;
     public int Quantity;
 
     // Getters
-    public Item getDetails() {return Details;}
-    public int getQuantity() {return Quantity;}
+    public Item getDetails() {
+        return Details;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
 
     // Setters
-    public void setDetails(Item details) {this.Details = details;}
-    public void setQuantity(int quantity) {this.Quantity = quantity;}
+    public void setDetails(Item details) {
+        this.Details = details;
+    }
+
+    public void setQuantity(int quantity) {
+        this.Quantity = quantity;
+    }
 
     // Constructor
     public InventoryItem(Item details, int Quantity) {
